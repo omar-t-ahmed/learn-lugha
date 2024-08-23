335acf4c96547270f1fa46306f1d8c455e0a3f83
Taha Khan <taha.khan.1@stonybrook.edu>
2024-08-23 17:20:42 -0400

    faq almost done

---
+++ src/app/faq/page.tsx
@@ -0,0 +1,49 @@
+import MaxWidthWrapper from "@/components/MaxWidthWrapper";
+import Navbar from "@/components/Navbar";
+
+export default function FAQ() {
+  return (
+    <div className="min-h-screen flex flex-col items-center bg-gradient-to-br from-black via-black to-purple-700 text-white">
+      <main className="flex flex-col items-center justify-center w-full max-w-7xl mx-auto px-6 py-10">
+        <h1 className="text-4xl md:text-5xl font-extrabold text-white mb-8">
+          Frequently Asked Questions
+        </h1>
+        <div className="w-full text-left text-gray-400 space-y-4">
+          <div className="bg-gray-900 p-4 rounded-lg shadow-md">
+            <h2 className="text-xl font-bold text-purple-500">What is LearnLugha?</h2>
+            <p className="mt-2">
+              LearnLugha is an AI-powered platform designed to help you learn Arabic in an engaging and effective way.
+            </p>
+          </div>
+          <div className="bg-gray-900 p-4 rounded-lg shadow-md">
+            <h2 className="text-xl font-bold text-purple-500">How does AI enhance my language learning experience?</h2>
+            <p className="mt-2">
+              Our AI adapts to your learning style, offering personalized feedback, identifying areas for improvement, and adjusting the difficulty of lessons.
+            </p>
+          </div>
+          <div className="bg-gray-900 p-4 rounded-lg shadow-md">
+            <h2 className="text-xl font-bold text-purple-500">When will LearnLugha be available?</h2>
+            <p className="mt-2">
+              We're currently in the final stages of development and plan to launch soon. By joining the waitlist, you'll be among the first to know when we go live.
+            </p>
+          </div>
+          <div className="bg-gray-900 p-4 rounded-lg shadow-md">
+            <h2 className="text-xl font-bold text-purple-500">Is LearnLugha suitable for beginners?</h2>
+            <p className="mt-2">
+              Absolutely! LearnLugha caters to all skill levels, from complete beginners to advanced learners. Our AI tailors lessons based on your starting point and learning progress.
+            </p>
+          </div>
+          <div className="bg-gray-900 p-4 rounded-lg shadow-md">
+            <h2 className="text-xl font-bold text-purple-500">What makes LearnLugha different from other language learning apps?</h2>
+            <p className="mt-2">
+              LearnLugha stands out due to its AI-driven approach, which provides a highly personalized learning experience. Unlike traditional apps, our platform continually adapts to your needs, making your journey to fluency smoother and more efficient.
+            </p>
+          </div>
+        </div>
+      </main>
+      <footer className="mb-5">
+        <p className="text-center text-gray-400">© 2024 LearnLugha. All rights reserved.</p>
+      </footer>
+    </div>
+  );
+}
--- src/components/Waitlist.tsx
+++ src/components/Waitlist.tsx
@@ -2,7 +2,7 @@
 
 import React, { useState } from "react";
 import db from "../../src/firebase";
-import { collection, addDoc } from "firebase/firestore";
+import { collection, addDoc, query, where, getDocs } from "firebase/firestore";
 
 export default function Waitlist() {
     const [email, setEmail] = useState("");
@@ -11,17 +11,24 @@ export default function Waitlist() {
     const handleSubmit = async (e: React.FormEvent) => {
         e.preventDefault();
         console.log("Form submitted");
-    
+
         try {
-            console.log("Attempting to add to Firestore");
-    
+
+            // Query Firestore to check if the email already exists
+            const q = query(collection(db, "waitlist"), where("email", "==", email));
+            const querySnapshot = await getDocs(q);
+
+            if (!querySnapshot.empty) {
+                setMessage("This email is already on the waitlist.");
+                return;
+            }
+
             // Add the email to Firestore
             const docRef = await addDoc(collection(db, "waitlist"), {
                 email: email,
                 timestamp: new Date(),
             });
-            console.log("Document successfully written with ID: ", docRef.id);
-    
+
             // Send the confirmation email via the API route
             const response = await fetch('/api/sendEmail', {
                 method: 'POST',
@@ -30,51 +37,50 @@ export default function Waitlist() {
                 },
                 body: JSON.stringify({ email }),
             });
-    
+
             const data = await response.json();
-    
+
             if (data.success) {
                 setMessage("Thank you! You've been added to the waitlist.");
             } else {
                 setMessage("There was an error sending the confirmation email.");
             }
-    
+
             setEmail(""); // Clear the email field after submission
-    
+
         } catch (error) {
             if (error instanceof Error) {
-                console.error("Error adding document: ", error.message);
                 setMessage("There was an error. Please try again.");
             } else {
-                console.error("Unexpected error: ", error);
                 setMessage("There was an unexpected error. Please try again.");
             }
         }
     };
-    
 
     return (
-        <div className="max-w-md mx-auto p-10 rounded-md shadow-md">
-            <h2 className="text-3xl font-bold text-white mb-6 text-center">
-                Join the Waitlist
+        <div className="max-w-lg mx-auto p-10 rounded-md">
+            <h2 className="text-2xl font-bold text-white mb-6 text-center">
+                Sign up for our waitlist and get <span className="font-bold text-purple-400">5 free lessons!</span>
             </h2>
             <form onSubmit={handleSubmit} className="flex flex-col">
-                <input
-                    type="email"
-                    className="p-3 border border-gray-600 bg-zinc-900 rounded-md mb-4 text-white"
-                    placeholder="Enter your email"
-                    value={email}
-                    onChange={(e) => setEmail(e.target.value)}
-                    required
-                />
-                <button
-                    type="submit"
-                    className="button-purple"
-                >
-                    Join Waitlist
-                </button>
+                <div className="flex rounded-md shadow-sm">
+                    <input
+                        type="email"
+                        className="p-3 border-t-2 border-b-2 border-l-2 border-gray-600 bg-zinc-900 text-white flex-grow rounded-l-md focus:outline-none"
+                        placeholder="Enter your email"
+                        value={email}
+                        onChange={(e) => setEmail(e.target.value)}
+                        required
+                    />
+                    <button
+                        type="submit"
+                        className="button-purple p-3 text-white font-bold bg-purple-500 rounded-r-md"
+                    >
+                        Sign Up
+                    </button>
+                </div>
             </form>
-            {message && <p className="mt-4 text-white">{message}</p>}
+            {message && <p className="mt-4 text-white text-center">{message}</p>}
         </div>
     );
 }
