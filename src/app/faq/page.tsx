1446b154dda3aa7080dd94bfd289c282494e3939
omar-t-ahmed <omartahmed@gmail.com>
2024-08-27 14:19:04 -0400

    add all

--- src/app/faq/page.tsx
+++ src/app/faq/page.tsx
@@ -11,76 +11,78 @@ export default function FAQ() {
   return (
     <div className="min-h-screen flex flex-col items-center bg-gradient-to-br from-black via-black to-purple-700 text-white">
       <main className="flex flex-col items-center justify-center w-full max-w-6xl mx-auto px-6 py-10">
-        <p className="text-2xl md:text-4xl p-4 font-extrabold text-white mb-8  rounded-md">
+        <p className="text-3xl md:text-4xl p-4 font-extrabold mb-8 text-transparent bg-clip-text bg-gradient-to-r from-purple-500 to-indigo-600 text-center">
           Frequently Asked Questions
         </p>
         <Accordion type="single" collapsible className="w-full text-left text-gray-400 space-y-4">
           <AccordionItem value="item-1">
-            <AccordionTrigger className="p-4 rounded-lg text-2xl font-bold text-purple-500">
+            <AccordionTrigger className="p-4 rounded-lg text-2xl font-bold text-white text-center md:text-left">
+              <p className="ml-6 md:ml-0">
               What is LearnLugha?
+              </p>
             </AccordionTrigger>
             <AccordionContent className="p-4 rounded-lg mt-2 text-lg">
-              <p className="mt-2">
+              <p className="mt-2 text-center md:text-left">
                 LearnLugha is more than just a language learning app—it&apos;s an AI-powered platform crafted to help you master Arabic in a way that&apos;s both engaging and highly effective. Whether you&apos;re a complete beginner or looking to refine your skills, LearnLugha offers an interactive experience that adapts to your needs and makes learning enjoyable.
               </p>
             </AccordionContent>
           </AccordionItem>
           <AccordionItem value="item-2">
-            <AccordionTrigger className="p-4 rounded-lg text-2xl font-bold text-purple-500">
+            <AccordionTrigger className="p-4 rounded-lg text-2xl font-bold text-white text-center md:text-left">
               How does AI enhance my language learning experience?
             </AccordionTrigger>
             <AccordionContent className="p-4 rounded-lg mt-2 text-lg">
-              <p className="mt-2">
+              <p className="mt-2 text-center md:text-left">
                 Our AI is designed to take your learning to the next level by tailoring the experience to suit your individual style. It offers personalized feedback, pinpoints areas where you can improve, and adjusts the difficulty of lessons based on your progress. This means that as you learn, the platform evolves with you, ensuring that you&apos;re always challenged, but never overwhelmed.
               </p>
             </AccordionContent>
           </AccordionItem>
           <AccordionItem value="item-3">
-            <AccordionTrigger className="p-4 rounded-lg text-2xl font-bold text-purple-500">
+            <AccordionTrigger className="p-4 rounded-lg text-2xl font-bold text-white text-center md:text-left">
               When will LearnLugha be available?
             </AccordionTrigger>
             <AccordionContent className="p-4 rounded-lg mt-2 text-lg">
-              <p className="mt-2">
+              <p className="mt-2 text-center md:text-left">
                 We&apos;re in the exciting final stages of development and aiming to launch very soon. By signing up for the waitlist, you&apos;ll be among the first to know when we go live, giving you early access to start your Arabic learning journey right away.
               </p>
             </AccordionContent>
           </AccordionItem>
           <AccordionItem value="item-4">
-            <AccordionTrigger className="p-4 rounded-lg text-2xl font-bold text-purple-500">
+            <AccordionTrigger className="p-4 rounded-lg text-2xl font-bold text-white text-center md:text-left">
               Is LearnLugha suitable for beginners?
             </AccordionTrigger>
             <AccordionContent className="p-4 rounded-lg mt-2 text-lg">
-              <p className="mt-2">
+              <p className="mt-2 text-center md:text-left">
                 Absolutely! LearnLugha is designed to cater to all skill levels, from complete beginners to advanced learners. If you&apos;re just starting your Arabic learning journey, our AI-driven platform will guide you through the basics with personalized lessons that focus on essential vocabulary, grammar, and pronunciation. The learning experience is interactive and adapts to your pace, ensuring that you build a strong foundation before moving on to more complex topics. As you progress, the AI continuously tailors the content to match your growing proficiency, making your learning experience both effective and enjoyable.
               </p>
             </AccordionContent>
           </AccordionItem>
           <AccordionItem value="item-5">
-            <AccordionTrigger className="p-4 rounded-lg text-2xl font-bold text-purple-500">
+            <AccordionTrigger className="p-4 rounded-lg text-2xl font-bold text-white text-center md:text-left">
               What makes LearnLugha different from other language learning apps?
             </AccordionTrigger>
             <AccordionContent className="p-4 rounded-lg mt-2 text-lg">
-              <p className="mt-2">
+              <p className="mt-2 text-center md:text-left">
                 LearnLugha is unique because it uses AI to create a truly personalized learning journey. Unlike traditional apps that offer a one-size-fits-all approach, our platform continuously adapts to your individual progress and learning style. This means that your path to fluency is not only smoother but also more efficient, as the content you encounter is always relevant to your current skill level and goals.
               </p>
             </AccordionContent>
           </AccordionItem>
           <AccordionItem value="item-6">
-            <AccordionTrigger className="p-4 rounded-lg text-2xl font-bold text-purple-500">
+            <AccordionTrigger className="p-4 rounded-lg text-2xl font-bold text-white text-center md:text-left">
               How do I track my progress with LearnLugha?
             </AccordionTrigger>
             <AccordionContent className="p-4 rounded-lg mt-2 text-lg">
-            <p className="mt-2">
-              As you use LearnLugha, you&apos;ll earn achievement badges as you reach new milestones, and receive personalized tips on areas where you can improve. These features help keep you engaged and on track to reach your language goals, making your learning experience both rewarding and effective.
-            </p>
+              <p className="mt-2 text-center md:text-left">
+                As you use LearnLugha, you&apos;ll earn achievement badges as you reach new milestones, and receive personalized tips on areas where you can improve. These features help keep you engaged and on track to reach your language goals, making your learning experience both rewarding and effective.
+              </p>
             </AccordionContent>
           </AccordionItem>
           <AccordionItem value="item-7">
-            <AccordionTrigger className="p-4 rounded-lg text-2xl font-bold text-purple-500">
+            <AccordionTrigger className="p-4 rounded-lg text-2xl font-bold text-white text-center md:text-left">
               How does the AI chatbot interface work in LearnLugha?
             </AccordionTrigger>
             <AccordionContent className="p-4 rounded-lg mt-2 text-lg">
-              <p className="mt-2">
+              <p className="mt-2 text-center md:text-left">
                 The AI chatbot in LearnLugha is like having a conversation partner available anytime you want to practice. It simulates real-life conversations that adjust to your skill level, allowing you to practice everything from basic greetings to more complex discussions. The chatbot can help you to quickly improve your language skills in a practical and engaging way.
               </p>
             </AccordionContent>
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -24,7 +24,7 @@ const Navbar = () => {
                     </div>
                 </div>
         </nav>
-    )
+    );
 };
 
 export default Navbar;
